b6a52f40100c070e9ed55a472f1b81d2f01c11e5
Elias Aggergaard Larsen <s235125@dtu.dk>
2024-11-21 01:52:39 +0100

    For now, simply shows average wattage on screen

--- src/ElPrices.cpp
+++ src/ElPrices.cpp
@@ -6,7 +6,6 @@
 
 #include <iostream>
 #include <thread>
-
 #include "Data/Usage/MockUsageGetter.h"
 
 
@@ -15,19 +14,8 @@ void ElPrices::launch()
     engineBase_ = std::make_shared<EngineBase>();
     usageController_ = std::make_shared<UsageController>();
     engineBase_->getGraphicsLibrary()->loadTexture("../Resources/number1.png");
-    int x = 250;
-    for (int i = 0; i < 4; i++)
-    {
-        priceDrawAbles_.push_back(std::make_shared<DrawAble>());
-        priceDrawAbles_.back()->setHeight(50);
-        priceDrawAbles_.back()->setWidth(50);
-        priceDrawAbles_.back()->setTextureIndex(1);
-
-        priceDrawAbles_.back()->setX(x);
-        priceDrawAbles_.back()->setY(250);
-        engineBase_->registerDrawAble(priceDrawAbles_.back());
-        x += 50;
-    }
+    currentPrice_ = std::make_shared<Text>("2.43 KR", 525,150,50);
+    engineBase_->registerText(currentPrice_);
     //engineBase_->registerUpdateFunction(update);
     //priceController_ = std::make_unique<PriceController>();
     //std::cout << priceController_->getDateFromString("18.11.2024")->getPriceAtPoint(22)->getPriceWithFees() << std::endl;
@@ -40,6 +28,7 @@ void ElPrices::wattHourUsed()
     std::cout << "Watt hour used" << std::endl;
     usageController_->wattHourUsed();
     std::cout << "Average usage: " + std::to_string(usageController_->getUsage()) << std::endl;
+    currentPrice_->setText(std::to_string(usageController_->getUsage()));
 }
 
 void ElPrices::update(float deltaTime)
